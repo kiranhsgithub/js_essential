57f4e03969872a34eef4139471471add754b1bdf
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-28 14:24:09 -0500

    logger and unary plus

---
+++ logger/log-types.js
@@ -0,0 +1,5 @@
+console.log("This is a default log message - no styles");
+console.debug("This is a debug log message - blue in chrome with no icon");
+console.info("This is an info message - black in chrome with an blue i icon");
+console.warn("This is a warning message - yellow in chrome with and exclamatory icon");
+console.error("This is an error message - red in chrome with a red cross icon");
---
+++ operations/unary-plus.js
@@ -0,0 +1,4 @@
+var a = 3;
+console.log("the value of ++a is " + ++a);//prints 4
+var b = 3;
+console.log("the value of b++ is " + b++);//prints 3
