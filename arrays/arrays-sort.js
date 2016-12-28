0427041c59d294960d33f72214b62bf9ba159434
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-28 16:43:58 -0500

    arrays sort

---
+++ arrays/arrays-sort.js
@@ -0,0 +1,11 @@
+var fruits = ['Mango', 'Apple', 'Banana','Watermelon',1, {name: "John"}, {"age": 28},[5,4,3,1],[3,2,3],['Pineapple','Grape','Orange'],['Mango','Apple','Banana'],[{name: "Doe"}, {"age": 18}],[1,"Jack",{age: 12}],["Dawson",5,{year: 2012}],[{id: 3003}, 'Rose', 14]];
+console.log("Before sort: " + JSON.stringify(fruits));
+
+for(var i=0; i <fruits.length; i ++){
+    console.log(" fruits[" + i +"] = " + JSON.stringify(fruits[i]));
+}
+fruits.sort();
+console.log("After sort: " + JSON.stringify(fruits));
+for(var i=0; i <fruits.length; i ++){
+    console.log(" fruits[" + i +"] = " + JSON.stringify(fruits[i]));
+}
