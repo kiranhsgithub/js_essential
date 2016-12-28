f5b701a48db45ed311f4034f318803c27312b74a
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-28 13:48:17 -0500

    break and continue

--- loops.js
+++ loops.js
@@ -11,3 +11,16 @@ for (var i=0; i <= 2; i++){
       break;  
     } 
 }
+
+for (var i=0; i <= 2; i++){
+    for (var j=0; j <= 3; j++) {
+        if(i == 1 && j == 2){
+            console.log("Missing Prints");
+            continue;
+        }
+        console.log("i = " + i +", j = " + j);
+    }
+	if(j != 4) {
+      break;  
+    } 
+}
