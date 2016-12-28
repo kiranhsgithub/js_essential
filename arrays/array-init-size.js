5d639cc26d391f8f2973a68aa09746e311c4a347
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-28 16:59:29 -0500

    arrays initial size

---
+++ arrays/array-init-size.js
@@ -0,0 +1,9 @@
+var a = new Array(5);
+var b = new Array(6,7);
+var c = [8];
+var d = [9,10];
+
+console.log("new Array(5)   : " + a);
+console.log("new Array(6,7) : " + b);
+console.log("[8]            : " + c);
+console.log("[9,10]         : " + d);
--- arrays/arrays-sort.js
+++ arrays/arrays-sort.js
@@ -1,4 +1,4 @@
-var fruits = ['Mango', 'Apple', 'Banana','Watermelon',9,1, {name: "John"}, {"age": 28},[5,4,3,1],[3,2,3],['Pineapple','Grape','Orange'],['Mango','Apple','Banana'],[{name: "Doe"}, {"age": 18}],undefined, null, -Infinity, Infinity, NaN, [null,1,"Jack",{age: 12}],["Dawson",5,{year: 2012}],[{id: 3003}, 'Rose', 14]];
+var fruits = ['Mango', 'Apple', 'Banana','Watermelon',9,1, {name: "John"}, {"age": 28},[5,4,3,1],[3,2,3],['Pineapple','Grape','Orange'],['Mango','Apple','Banana'],[{name: "Doe"}, {"age": 18}],undefined, null, 4/0, -4/0, 'r' * 6, [null,1,"Jack",{age: 12}],["Dawson",5,{year: 2012}],[{id: 3003}, 'Rose', 14]];
 console.log("Before sort: " + JSON.stringify(fruits));
 
 for(var i=0; i <fruits.length; i ++){
