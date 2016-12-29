f1d6e3ee0d809adef273fe0cacc7362336077dca
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-28 19:03:19 -0500

    arrays reverse concat

---
+++ arrays/arrays-join-concat.js
@@ -0,0 +1,12 @@
+var array1 = [10,30,40];
+
+
+console.log("Array 1 Join : " + array1.join('|'));
+
+var array2 = [20,50,60];
+var joinedArray = array1.concat(array2);
+
+console.log("Array 1 : " + array1);
+console.log("Array 2 : " + array2);
+
+console.log("joinedArray : " + joinedArray);
---
+++ arrays/arrays-reverse.js
@@ -0,0 +1,6 @@
+var simpleArray = [10,20,30];
+
+console.log("Original Array : " + simpleArray);
+var reversedArray = simpleArray.reverse();
+console.log("Original Array : " + simpleArray);
+console.log("Reversed Array : " + reversedArray);
