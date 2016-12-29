2c3d39ef88b5c6b69f0ae2a88a18c8f34ee02481
kiranhsgithub <hskiran.bms@gmail.com>
2016-12-29 00:18:42 -0500

    string case conversion

---
+++ strings/case-conversion.js
@@ -0,0 +1,10 @@
+var firstName = "John";
+var lastName = "Doe";
+
+var firstUpper = firstName.toUpperCase();
+var lastLower = lastName.toLowerCase();
+
+console.log("Original String  : " + firstName);//firstName remains unchanged
+console.log("Uppercase String : " + firstUpper);
+console.log("Original String  : " + lastName);//lastName remains unchanged
+console.log("Lowercase String : " + lastLower);
