473061cef23c84655043975ce4ecf9ca0b257bd6
kiranhsgithub <hskiran.bms@gmail.com>
2017-01-03 13:33:28 -0500

    dom manipulation

--- Ex_Files_JS_Essential/Chap05/AccessingDOM/script.js
+++ Ex_Files_JS_Essential/Chap05/AccessingDOM/script.js
@@ -1,2 +1,19 @@
+var mainTitle = document.getElementById("mainTitle");
 
+console.log("This is an element of type: ", mainTitle.nodeType);
+console.log("The inner HTML is: ", mainTitle.innerHTML);
+console.log("Child nodes: ", mainTitle.childNodes.length);
+
+var myLinks = document.getElementsByTagName("a");
+console.log("Links: ", myLinks.length);
+
+
+var mainContent = document.getElementById("mainContent");
+mainContent.setAttribute("align","right");
+
+var mainTitle = document.getElementById("mainTitle");
+console.log(mainTitle.innerHTML);
+
+var sidebar = document.getElementById("sidebar");
+console.log(sidebar.innerHTML);
 
