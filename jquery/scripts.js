1ec82e054afecf96b4014141ea94d7d975c063f5
kiranhsgithub <hskiran.bms@gmail.com>
2017-01-03 20:24:07 -0500

    fade out

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -1,11 +1,14 @@
 $("#myDiv").addClass("highlight");
 $("li:last").addClass("highlight");
 $("p:not(:contains('specified'))").removeClass("highlight");
-$("#fiveSeconds").hide(5000);
 $("#clickMe").click(function () {
     $("#clickMe").text("You clicked me");
 });
 $("#clickMeUsingThis").click(function () {
     $("#clickMeUsingThis").text("You clicked me and text is changed using \"this\"");
+});
+$("#fiveSeconds").hide(5000);
+$("#fiveSecondsFadeout").click(function() {
+    $(this).fadeOut(5000);
 });
 
