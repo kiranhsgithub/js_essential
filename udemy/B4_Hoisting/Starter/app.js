4ec2374114a34a5b2899656993f310e27c8fe004
kiranhsgithub <hskiran.bms@gmail.com>
2017-01-07 03:29:44 -0500

    hoisting

--- udemy/B4_Hoisting/Starter/app.js
+++ udemy/B4_Hoisting/Starter/app.js
@@ -1,5 +1,16 @@
+b();
+console.log(a);
+//c();
+
 var a = 'Hello World!';
 
 function b() {
-    
+    console.log("Called b!");
 }
+
+var c = function() {
+    console.log("Called c!");
+}
+c();
+b();
+console.log(a);
