bd3ee6ae0ffc3bc2d09a6e808e884eddc8b11b05
kiranhsgithub <hskiran.bms@gmail.com>
2017-01-10 01:06:44 -0500

    lexical scope

--- udemy/B10_ScopeChain/Starter/app.js
+++ udemy/B10_ScopeChain/Starter/app.js
@@ -1,11 +1,20 @@
 function b() {
-	console.log(myVar);
+	console.log("inside b: ",myVar);
 }
 
 function a() {
 	var myVar = 2;
 	b();
+    
+    function c() {
+        console.log("inside c:",myVar);
+    }
+    c();
+    console.log("inside a:",myVar);
+    
 }
 
 var myVar = 1;
 a();
+console.log("outside:",myVar);
+
